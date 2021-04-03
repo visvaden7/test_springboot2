a509232fc70275f21d802816ad16723ecd106246
visvaden7 <visvaden7@naver.com>
2021-04-04 00:08:48 +0900

    p150 mustache 전체조회화면 만들기

--- src/main/java/com/gldnif/test/service/posts/PostsService.java
+++ src/main/java/com/gldnif/test/service/posts/PostsService.java
@@ -2,12 +2,16 @@ package com.gldnif.test.service.posts;
 
 import com.gldnif.test.domain.posts.Posts;
 import com.gldnif.test.domain.posts.PostsRepository;
+import com.gldnif.test.web.dto.PostsListResponseDto;
 import com.gldnif.test.web.dto.PostsResponseDto;
 import com.gldnif.test.web.dto.PostsSaveRequestDto;
 import com.gldnif.test.web.dto.PostsUpdateRequestDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @RequiredArgsConstructor
 @Service
@@ -31,5 +35,11 @@ public class PostsService {
         Posts entity = postsRepository.findById(id)
                 .orElseThrow(()->new IllegalArgumentException("해당 사용자가 없습니다. id=" + id));
         return new PostsResponseDto(entity);
+    }
+    @Transactional(readOnly = true)
+    public List<PostsListResponseDto> findAllDesc(){
+        return postsRepository.findAllDesc().stream()
+                .map(PostsListResponseDto::new)
+                .collect(Collectors.toList());
     }
 }
--- src/main/java/com/gldnif/test/web/IndexController.java
+++ src/main/java/com/gldnif/test/web/IndexController.java
@@ -1,13 +1,26 @@
 package com.gldnif.test.web;
 
+import com.gldnif.test.service.posts.PostsService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+@RequiredArgsConstructor
 @Controller
 public class IndexController {
 
+    private final PostsService postsService;
+
     @GetMapping("/")
-    public String index(){
+    public String index(Model model){
+        model.addAttribute("posts",postsService.findAllDesc());
         return "index";
     }
+
+    @GetMapping("/posts/save")
+    public String postsSave(){
+        return "posts-save";
+    }
+
 }
