574711a6eaf7b9181b6fdc9c69a0da3a028739bc
visvaden7 <visvaden7@naver.com>
2021-04-08 15:17:02 +0900

    p194 aouth2 구글 로그인 API 기능 추가

--- src/main/java/com/gldnif/test/web/IndexController.java
+++ src/main/java/com/gldnif/test/web/IndexController.java
@@ -1,5 +1,6 @@
 package com.gldnif.test.web;
 
+import com.gldnif.test.config.auth.dto.SessionUser;
 import com.gldnif.test.service.posts.PostsService;
 import com.gldnif.test.web.dto.PostsResponseDto;
 import lombok.RequiredArgsConstructor;
@@ -8,15 +9,24 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
+import javax.mail.Session;
+import javax.servlet.http.HttpSession;
+
 @RequiredArgsConstructor
 @Controller
 public class IndexController {
 
     private final PostsService postsService;
+    private final HttpSession httpSession;
 
     @GetMapping("/")
     public String index(Model model){
         model.addAttribute("posts",postsService.findAllDesc());
+        SessionUser user = (SessionUser) httpSession.getAttribute("user");
+
+        if(user != null){
+            model.addAttribute("userName" , user.getName());
+        }
         return "index";
     }
 
