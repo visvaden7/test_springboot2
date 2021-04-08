34334ecc1ece4feccf471f5eb4992e73a2a80e69
visvaden7 <visvaden7@naver.com>
2021-04-08 16:17:20 +0900

    p194 aouth2 구글 로그인 API 기능 추가(@loginUser로 session 값을 받아오는 방식 변경

--- src/main/java/com/gldnif/test/web/IndexController.java
+++ src/main/java/com/gldnif/test/web/IndexController.java
@@ -1,5 +1,6 @@
 package com.gldnif.test.web;
 
+import com.gldnif.test.config.auth.LoginUser;
 import com.gldnif.test.config.auth.dto.SessionUser;
 import com.gldnif.test.service.posts.PostsService;
 import com.gldnif.test.web.dto.PostsResponseDto;
@@ -9,20 +10,15 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
-import javax.mail.Session;
-import javax.servlet.http.HttpSession;
-
 @RequiredArgsConstructor
 @Controller
 public class IndexController {
 
     private final PostsService postsService;
-    private final HttpSession httpSession;
 
     @GetMapping("/")
-    public String index(Model model){
+    public String index(Model model, @LoginUser SessionUser user){
         model.addAttribute("posts",postsService.findAllDesc());
-        SessionUser user = (SessionUser) httpSession.getAttribute("user");
 
         if(user != null){
             model.addAttribute("userName" , user.getName());
